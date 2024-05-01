a2174d0f28dcd2aeb9a41a461ea8560ee632fc95
velican <1201396@student.roc-nijmegen.nl>
2024-05-01 14:58:55 +0200

    heb een inventory table gemaakt

---
+++ prisma/migrations/20240501125743_database/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "student_number" INTEGER NOT NULL,
+    "class_name" TEXT NOT NULL,
+    "pass_id" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "inventory" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL
+);
