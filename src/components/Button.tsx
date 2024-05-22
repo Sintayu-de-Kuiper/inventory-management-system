33dee9e99668378d2e6d0093632e18dfa301da80
Rutger <1201954@student.roc-nijmegen.nl>
2024-05-22 10:08:29 +0200

    Add hover effect to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 export default function Button({ children, className, ...props }: ButtonProps) {
   return (
     <button
-      className={`bg-accent text-primary rounded-full px-8 py-2 text-xl font-medium ${className}`}
+      className={`bg-accent hover:bg-accent-hover text-primary rounded-full px-8 py-2 text-xl font-medium ${className}`}
       {...props}
     >
       {children}
