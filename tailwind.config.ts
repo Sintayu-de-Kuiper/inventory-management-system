4abe1444c017f6d0249d0ac99af55c16b3ceb0e9
Rutger <1201954@student.roc-nijmegen.nl>
2024-05-22 09:28:58 +0200

    Add colors to config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      primary: "#ffffff",
+      secondary: "#000000",
+      accent: "#6E71B6",
+      gray: "#F2F2F2",
     },
   },
   plugins: [],
