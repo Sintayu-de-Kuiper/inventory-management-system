135ab04c09450c9b0e78a718d52f035b1d3779a0
Rutger <1201954@student.roc-nijmegen.nl>
2024-05-24 11:51:29 +0200

    Add Make padding on button larger. Add back button

--- src/app/return/page.tsx
+++ src/app/return/page.tsx
@@ -1,12 +1,22 @@
 import ReturnItemsForm from "@/components/return/ReturnItemsForm";
 import Button from "@/components/Button";
+import BackButton from "@/components/BackButton";
+import Link from "next/link";
 
 export default function ReturnItemsPage() {
   return (
-    <main className={"grid min-h-screen grid-rows-[1fr_3fr_auto] py-12"}>
+    <main
+      className={
+        "relative grid min-h-screen grid-rows-[2fr_7fr_auto] gap-10 px-10 py-12"
+      }
+    >
       <h1 className={"text-center text-5xl"}>Return Items</h1>
       <ReturnItemsForm />
-      <Button className={"mx-auto"}>Inleveren</Button>
+      <Button className={"mx-auto w-fit"}>Inleveren</Button>
+
+      <BackButton>
+        <Link href={"/home"}>Klaar</Link>
+      </BackButton>
     </main>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 export default function Button({ children, className, ...props }: ButtonProps) {
   return (
     <button
-      className={`text-accent-foreground w-fit rounded-full bg-accent px-8 py-2 text-xl font-medium hover:bg-accent-hover ${className}`}
+      className={`text-accent-foreground rounded-full bg-accent px-12 py-2 text-xl font-medium hover:bg-accent-hover ${className}`}
       {...props}
     >
       {children}
