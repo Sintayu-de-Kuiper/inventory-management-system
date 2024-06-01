b631cee9efbfc1dee528e817a57d34cd6daceca5
Rutger <rutger.pronk11@gmail.com>
2024-06-01 12:26:18 +0200

    Create login form and make page server component. Log users out when they are on the home page

--- src/app/login.ts
+++ src/app/login.ts
@@ -4,8 +4,12 @@ import { signIn } from "@/auth";
 
 export async function login(studentNumber: number) {
   "use server";
-  return signIn("credentials", {
-    studentNumber,
-    redirect: false,
-  });
+  try {
+    return await signIn("credentials", {
+      studentNumber,
+      redirect: false,
+    });
+  } catch (error) {
+    return null;
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,17 @@
-"use client";
+import LoginForm from "@/components/login/LoginForm";
+import { auth, signOut } from "@/auth";
 
-import { FormEvent } from "react";
-import { useRouter } from "next/navigation";
-import Button from "@/components/Button";
-import cuid from "cuid";
-import { login } from "@/app/login";
+export default async function LoginPage() {
+  const session = await auth();
+  console.log(session);
 
-export default function LoginPage() {
-  const router = useRouter();
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
-    const studentNumber = parseInt(formData.get("studentNumber") as string);
-
-    const user = await login(studentNumber);
-
-    if (!user) {
-      // This should be a real passId soon. Now not implemented and id is generated.
-      router.push(`/register/${cuid()}/${studentNumber}`);
-    } else {
-      router.push("/home");
-    }
-  };
+  if (!session) {
+    await signOut();
+  }
 
   return (
     <main className="from-gray-200 to-gray-100 flex h-screen min-h-screen flex-col items-center justify-center bg-gradient-to-b">
-      <form
-        onSubmit={handleSubmit}
-        className=" flex w-64 flex-col items-center "
-      >
-        <div>
-          <label
-            htmlFor="studentnummer"
-            className={"text-lg text-gray-foreground"}
-          >
-            Studentnummer
-          </label>
-          <input
-            id="studentNumber"
-            name="studentNumber"
-            type="number"
-            className="form__field h-12 w-full p-2 text-lg outline-none"
-            required
-          />
-        </div>
-        <Button className={"mt-10"}>Login</Button>
-      </form>
+      <LoginForm />
     </main>
   );
 }
